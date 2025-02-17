c6c79c53ab584ea53ac2ed69539d11bed2093163
Cleyton Muto <cleytonmuto@gmail.com>
2025-02-17 16:23:08 -0300

    chore: answer to max connections challenge

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,31 @@
 'use strict';
 
-import database from "infra/database.js";
+import database from 'infra/database.js';
 
 const status = async (req, res) => {
-  const result = await database.query('SELECT 2 + 3;');
-  console.log(result.rows);
-  res.status(200).json({ 'chave': 'são acima da média' });
+  const updatedAt = new Date().toISOString();
+
+  // desafio
+  const version = await database.query('SHOW server_version;');
+  const formatVersion = version.rows[0].server_version;
+
+  const maxConnections = await database.query('SHOW max_connections;');
+  const formatMaxConnections = Number(maxConnections.rows[0].max_connections);
+
+  const usedConnections = await database.query("SELECT COUNT(*) FROM pg_stat_activity WHERE state='active';");
+  const formatUsedConnections = Number(usedConnections.rows[0].count);
+
+  res.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: formatVersion,
+        max_connections: formatMaxConnections,
+        used_connections: formatUsedConnections
+      }
+    }
+  });
+
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,21 @@
-test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+'use strict';
+
+test('GET to /api/v1/status should return 200', async () => {
+  const response = await fetch('http://localhost:3000/api/v1/status');
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependencies.database.version).toBeDefined();
+  expect(responseBody.dependencies.database.version).toEqual('16.6');
+
+  expect(responseBody.dependencies.database.max_connections).toBeDefined();
+  expect(responseBody.dependencies.database.max_connections).toBeGreaterThan(0);
+
+  expect(responseBody.dependencies.database.used_connections).toBeDefined();
+  expect(responseBody.dependencies.database.used_connections).toBeGreaterThan(0);
 });
